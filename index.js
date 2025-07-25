73777cb3c09c44f11e8ca69fa0138e7f0cc4dd94
josiahh <eyosiyas62@gmail.com>
2025-07-24 23:56:07 -0700

    small change to trigger deploy

--- index.js
+++ index.js
@@ -2,11 +2,9 @@ import 'dotenv/config';
 import {startBot,bot} from "./src/bot.js"
 import { checkAndSendReminders } from './src/reminderScheduler.js';
 
-console.log('DEBUG: BOT_TOKEN from process.env:', process.env.BOT_TOKEN ? 'Token found (length: ' + process.env.BOT_TOKEN.length + ')' : 'Token NOT found or is empty');
-startBot();
-
 const REMINDER_CHECK_INTERVAL_MS = 60 * 1000; 
 
 checkAndSendReminders(bot);
 setInterval(() => checkAndSendReminders(bot), REMINDER_CHECK_INTERVAL_MS);
 console.log(`Reminder scheduler started, checking every ${REMINDER_CHECK_INTERVAL_MS / 1000} seconds.`);
+// Small change to trigger deploy
